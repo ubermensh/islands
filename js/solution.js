506c616bac62eef7ae2289263b2bd87e8335fbde
AlexShpil <alexshpil@gmail.com>
2018-01-20 13:53:12 +0300

    test1

--- js/solution.js
+++ js/solution.js
@@ -10,7 +10,34 @@
      * @returns {number} кол-во островов
      */
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
+        var islands = 0,
+            map_heights = map.length,
+            map_lenghts = map[0].lenghts;
+            
+        for (var i = 0; i < map_heights; i++) {
+            for (var j = 0; j < map_lenghts; j++) {
+                if (map[i][j] === 1) {
+                    if (i === 0 and j === 0) { 
+                        islands += 1;
+                        map[i][j] = 2;}
+                    elif ( i === 0 and map[i][j-1] !== 2) {
+                        islands += 1;
+                        map[i][j] = 2;}
+                    elif ( i === 0 and map[i][j-1] === 2) {
+                        map[i][j] = 2;}
+                    elif ( i !== 0 and j === 0 map[i-1][j] !==2 ) {
+                        islands += 1;
+                        map[i][j] = 2;}
+                    elif ( i !== 0 and j === 0 and map[i-1][j] ===2) {
+                        map[i][j] = 2;}
+                    elif ( i !== 0 and j !== 0 ) {
+                        if ( map[i-1][j] == 2 or map [i][j-1] == 2 or map[i][j+1] == 1 ) {
+                            map[i][j] = 2;}
+                        else { islands += 1;
+                              map[i][j] = 2;}
+                    }
+                }
+            }
         return 0;
     }
 
