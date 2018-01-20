f0af2862402ecfa45c4d83dac9927cc117919c72
AlexShpil <alexshpil@gmail.com>
2018-01-20 15:05:30 +0300

    test

--- js/solution.js
+++ js/solution.js
@@ -19,16 +19,16 @@
             for (var j = 0; j < map_lenghts; j++) {
                 if (map[i][j] === 1) {
                     if (i === 0 and j === 0) { 
-                        islands += 1;
+                        islands++;
                         found_coordinates.push([i,j]);}
                     elif ( i === 0 and [i,j-1] not in found_coordinates) {
-                        islands += 1;
+                        islands++;
                         found_coordinates.push([i,j]);}
                     elif ( i === 0 and [i,j-1] in found_coordinates) {
                         found_coordinates.push([i,j]);}
                     else {
 						if ( j === 0 and [i-1,j] not in found_coordinates and map[i][j+1] !== 1 ) {
-							islands += 1;
+							islands++;
 							found_coordinates.push([i,j]);}
 						elif ( j === 0 and [i-1,j] in found_coordinates) {
 							found_coordinates.push([i,j]);}
@@ -36,7 +36,7 @@
 							if ( [i-1,j] in found_coordinates or [i,j-1] in found_coordinates or map[i][j+1] === 1 ) {
 								found_coordinates.push([i,j]);}
 							else { 
-								islands += 1;
+								islands++;
 								found_coordinates.push([i,j]);}
 						}
 					}
