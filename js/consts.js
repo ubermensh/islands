8b2d708fd8310ea11891f93be301b550014392b9
ubermensh <lemantar.flcl@gmail.com>
2018-01-31 14:35:12 +0300

    new solution

--- js/consts.js
+++ js/consts.js
@@ -12,5 +12,6 @@
         [0, 1, 0, 0]
     ];
 
+    console.log(SHRI_ISLANDS.MAP);
     root.SHRI_ISLANDS = SHRI_ISLANDS;
 })(this);
--- js/solution.js
+++ js/solution.js
@@ -8,39 +8,59 @@
      *
      * @param {number[][]} map карта островов представленная двумерной матрицей чисел
      * @returns {number} кол-во островов
+     *
      */
     function solution(map) {
-        console.log('sol', map);
-        var islands = 0,
-			map_c = [],
-			found_coordinates = [],
-            map_heights = map.length,
-            map_lenghts = map[0].length;
-console.log('sol', map_heights, map_lenghts );
+			const _map =_copyMatrix(map); 
+        let islandsCount = 0;
+        let yCoordinate = 0;
+        let xCoordinate = 0;
         
-		map_c = map;
+        _map.map((row) => {
+            yCoordinate += 1;
+            row.map((val) =>{
+                xCoordinate +=1;
+                isIsland(val);
+            })
+        });
+        function isIsland(val) {
+            if (val == 1 && !isIslandCounted() ){
+               islandsCount +=1;  
+            }
+        };
+        //recursive
+        function isIslandCounted(val){
+            //island is already counted if it has parts ABOVE or LEFT 
+            //get point ABOVE 
+            //get point LEFT
+            return 
+
+            
+
+        }
+
         for (var i = 0; i < map_heights; i++) {
             for (var j = 0; j < map_lenghts; j++) {
                 if (map_c[i][j] === 1) {
                     if (i === 0 && j === 0) { 
                         islands++;
-                        map_c[i][j] = 2;}
-                    else if ( i == 0 && map_c[i][j-1] != 2) {
+                        map_c[i][j] = 'x';}
+                    else if ( i == 0 && map_c[i][j-1] != 'x') {
                         islands++;
-                        map_c[i][j] = 2;}
-                    else if ( i == 0 && map_c[i][j-1] == 2 ) {
-                        map_c[i][j] = 2;}
+                        map_c[i][j] = 'x';}
+                    else if ( i == 0 && map_c[i][j-1] == 'x' ) {
+                        map_c[i][j] = 'x';}
                     else {
-						if ( j == 0 && map_c[i-1][j] != 2 && map[i][j+1] != 1 ) {
+						if ( j == 0 && map_c[i-1][j] != 'x' && map_b[i][j+1] != 1 ) {
 							islands++;
-							map_c[i][j] = 2;}
-						else if ( j == 0 && map_c[i-1][j] == 2 ) {
-							map_c[i][j] = 2;}
-						else if ( map_c[i-1][j] == 2 || map[i][j-1] == 2 || map_c[i][j+1] == 1) {
-						map_c[i][j] = 2;}
+							map_c[i][j] = 'x';}
+						else if ( j == 0 && map_c[i-1][j] == 'x' ) {
+							map_c[i][j] = 'x';}
+						else if ( map_c[i-1][j] == 'x' || map_b[i][j-1] == 'x' || map_c[i][j+1] == 1) {
+						map_c[i][j] = 'x';}
 						else {
 								islands++;
-								map_c[i][j] = 2;}
+								map_c[i][j] = 'x';}
 						
 					}
 				}
@@ -49,6 +69,12 @@ console.log('sol', map_heights, map_lenghts );
         console.log('sol',islands );
 			
         return islands;
+    }
+    function _copyMatrix(matrix) {
+        var result = matrix.map(function(arr) {
+            return arr.slice();
+        });
+        return result;
     }
 
     root.SHRI_ISLANDS.solution = solution;
